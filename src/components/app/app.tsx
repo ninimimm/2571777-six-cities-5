c0d9141ddfcf604cfd6905581ba7275820b26283
Nikita Chernov <nikitachernov89221156246@yandex.ru>
2024-10-17 14:02:54 +0500

    complete task

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,40 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import OfferScreen from '../../pages/offer-screen/offer-screen';
+import PageNotFound from '../../pages/page-not-found/page-not-found';
 import WelcomeScreen from '../../pages/welcome-screen/welcome-screen';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import PrivateRoute from './private-route';
 
 type AppScreenProps = {
   rentalOffersNumber: number;
 };
 
 function App({ rentalOffersNumber }: AppScreenProps): JSX.Element {
-  return <WelcomeScreen rentalOffersNumber={rentalOffersNumber} />;
+  const authorizationStatus = AuthorizationStatus.NoAuth;
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<WelcomeScreen rentalOffersNumber={rentalOffersNumber} />}
+        />
+        <Route path={AppRoute.Login} element={<LoginScreen />} />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authorizationStatus={authorizationStatus}>
+              <FavoritesScreen />
+            </PrivateRoute>
+          }
+        />
+        <Route path={AppRoute.Offer} element={<OfferScreen />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/const.ts
@@ -0,0 +1,12 @@
+export enum AppRoute {
+	Main = '/',
+	Login = '/login',
+	Favorites = '/favorites',
+	Offer = '/offer'
+}
+
+export enum AuthorizationStatus {
+	Auth = 'AUTH',
+	NoAuth = 'NO_AUTH',
+	Unknown = 'UNKNOWN'
+}
